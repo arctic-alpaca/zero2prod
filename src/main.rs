f5ee03b69fa37790056b8e83b3f2dbc719f3da7c
arctic-alpaca <67190338+arctic-alpaca@users.noreply.github.com>
2021-02-21 18:15:48 +0100

    3.3 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!1");
+use actix_web::{get, route, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(_: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
